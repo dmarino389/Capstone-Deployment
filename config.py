36dad736c7995208f0704436f08c2522e0466a87
dmarino389 <139820583+dmarino389@users.noreply.github.com>
2024-02-02 06:26:11 -0600

    Done

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
 import os
 
-# basedir = os.path.abspath(os.path.dirname(__name__))
-
 class Config():
   FLASK_APP = os.environ.get("FLASK_APP")
   FLASK_DEBUG = os.environ.get("FLASK_DEBUG")
   SECRET_KEY = os.environ.get("SECRET_KEY")
-  # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-  SQLALCHEMY_DATABASE_URI = 'postgres://hpawltfl:y5MqwxxP2LT0qR3hzR1jBPgVAohkckYJ@jelani.db.elephantsql.com/hpawltfl'
+  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
   SQLALCHEMY_TRACK_MODIFICATIONS = False
